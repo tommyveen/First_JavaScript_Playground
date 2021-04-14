ff2e91740507fb4aad40c77a9ac3744c64d63ce2
Tommy Veen <75741894+tommyveen@users.noreply.github.com>
2021-04-14 13:41:04 +0200

    Add files via upload

---
+++ First JavaScript/main.js
@@ -0,0 +1,19 @@
+console.log("Hello Winc Academy");
+let name = 'Tommy';
+console.log(name);
+let numbers = 4 + 4;
+let strings = '4' + '4';
+console.log(numbers);
+console.log(strings);
+let addition = 1 + 2;
+let substraction = 1 - 2;
+let multiplication = 1 * 2;
+let division = 1 / 2;
+let modulus = 1 % 2
+console.log(addition);
+console.log(substraction);
+console.log(multiplication);
+console.log(division);
+console.log(modulus);
+let age = 'zevenentwintig';
+console.log(typeof age);
